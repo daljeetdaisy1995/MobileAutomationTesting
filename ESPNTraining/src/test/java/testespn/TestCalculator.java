f0727e213729cc343f3a7f2638591382d017b3f1
Daljeet Kaur <daljeet@Daljeets-MacBook-Pro.local>
2019-06-10 15:47:49 -0400

    commit

---
+++ ESPNTraining/src/test/java/testespn/TestCalculator.java
@@ -0,0 +1,27 @@
+package testespn;
+
+import base.MobileAPI2;
+import espn.Calculator;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+
+//public class TestCalculator  {
+   /* Calculator calculator;
+    @BeforeMethod
+    public void initialization(){
+        calculator = PageFactory.initElements(AndroidDriver, Calculator.class);
+    }
+
+    @Test(enabled = true)
+    public void testAddition() throws MalformedURLException {
+        calculator.setCapabilities();
+        calculator.addition();
+
+    }
+
+}*/
